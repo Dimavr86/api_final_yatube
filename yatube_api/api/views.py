70047c7cded85d9623874dbc083ef4eaf2a41955
Дмитрий Семёнов <semdim@mail.ru>
2022-03-02 22:49:25 +0800

    ready for review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,66 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from rest_framework import filters, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Group, Post
+from .permissions import IsAuthor
+from .serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer
+)
+
+
+User = get_user_model()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    permission_classes = [IsAuthor]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        queryset = post.comments
+        return queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.following.all()
+
+    def perform_create(self, serializer):
+        username = serializer.validated_data.get('following')
+        following = get_object_or_404(User, username=username)
+        user = self.request.user
+        serializer.save(
+            user=user,
+            following=following
+        )
