fea4b6ed2bfdf1748d1998d3817011ddca170670
Дмитрий Семёнов <semdim@mail.ru>
2022-03-05 23:35:00 +0800

    corrected according with first review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -45,9 +45,9 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(author=self.request.user, post=post)
 
 
-class GroupViewSet(viewsets.ReadOnlyModelViewSet): 
-    serializer_class = GroupSerializer 
-    queryset = Group.objects.all() 
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
 
 
